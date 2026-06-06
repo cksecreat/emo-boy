9c03965256cdb1cd6ee1892f8342f8907473af56
cksecreat <cksecreat@gmail.com>
2026-06-06 23:18:39 +0800

    Update bot.js

--- src/config/bot.js
+++ src/config/bot.js
@@ -90,7 +90,7 @@ export const botConfig = {
     colors: {
       // Main brand colors.
       primary: "#380E12", 
-      secondary: "#2F3136", 
+      secondary: "#BA1E1D", 
 
       // Standard status colors for success/error/warning/info messages.
       success: "#57F287", 
